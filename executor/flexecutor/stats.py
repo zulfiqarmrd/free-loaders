90c19534254c264baf29345bf9e9c28bf8d2c48f
Marshall Clyburn <mardev2013@gmail.com>
2022-10-20 08:43:35 -0400

    Add support for NVIDIA Jetson boards
    Use the jetson-stats module to get GPU information. Add a separate
    requirements file for the dependencies for those boards.

--- executor/flexecutor/stats.py
+++ executor/flexecutor/stats.py
@@ -1,4 +1,5 @@
 import functools
+import os
 
 import psutil
 
@@ -18,13 +19,14 @@ def fetch():
     # Get consistent views of these once.
     mem = psutil.virtual_memory()
     (proc_count, thread_count) = _process_thread_count()
+    gpu_stats = _gpu_stats()
 
     state = {
         'cpu-load': psutil.getloadavg()[0],
         'free-ram': mem.available,
         'total-ram': mem.total,
-        'free-vram': 0, # TODO
-        'total-vram': 0, # TODO
+        'free-vram': gpu_stats['free-vram'],
+        'total-vram': gpu_stats['total-vram'],
         'process-count': proc_count,
         'thread-count': thread_count,
     }
@@ -40,3 +42,28 @@ def _process_thread_count():
                           0)
 
     return (process_count, thread_count)
+
+
+def _gpu_stats():
+    '''Fetch GPU usage information, if available.
+
+    Has special handling for NVIDIA Jetson SBCs.
+    '''
+
+    stats = {
+        'usage': 0,
+        'free-vram': 0,
+        'total-vram': 0,
+    }
+
+    # Look at the kernel build to tell if this is a Jetson SBC.
+    if os.uname().release.endswith('-tegra'):
+        import jtop
+        with jtop.jtop() as jetson_ctx:
+            if jetson_ctx.ok():
+                stats['usage'] = jetson_ctx.stats['GPU']
+                # Note: SoC shares RAM between CPU and GPU.
+                stats['free-vram'] = jetson_ctx.stats['RAM']
+                stats['total-vram'] = psutil.virtual_memory().total
+
+    return stats
