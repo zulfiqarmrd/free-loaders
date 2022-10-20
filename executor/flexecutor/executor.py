7338c8568fa56cbe119557f352f7cd3244e18193
Marshall Clyburn <mardev2013@gmail.com>
2022-10-20 15:08:06 -0400

    Fix runtime errors

--- executor/flexecutor/executor.py
+++ executor/flexecutor/executor.py
@@ -37,7 +37,7 @@ def __executor_entry(controller_addr, executor_id):
 
     log.i('started')
 
-    mqtt_client = paho.mqtt.client.Client()
+    mqtt_client = paho.mqtt.client.Client(userdata={'executor_id': executor_id})
     mqtt_client.on_connect = __mqtt_on_connect
     mqtt_client.on_message = __mqtt_message_received
     mqtt_client.on_disconnect = __mqtt_on_disconnect
@@ -45,11 +45,13 @@ def __executor_entry(controller_addr, executor_id):
     log.i('connecting to {}:{}'.format(controller_addr, MQTTServerPort))
     while True:
         try:
-            mqtt_client.connect(controller_addr, MQTTServerPort)
+            mqtt_client.connect(controller_addr, MQTTServerPort, 60)
+            break
         except ConnectionRefusedError:
             log.e('connection refused')
             time.sleep(2)
 
+    log.d('entering MQTT client loop')
     mqtt_client.loop_forever()
 
 def __mqtt_on_connect(client, userdata, flags, rc):
@@ -58,23 +60,34 @@ def __mqtt_on_connect(client, userdata, flags, rc):
 
 def __mqtt_on_disconnect(client, userdata, rc=0):
     log.w('Disconnected from server: {}'.format(rc))
+    client.loop_stop()
 
 def __mqtt_message_received(client, data, msg):
-    log.i('Received message ({})'.format(msg.topic))
+    log.i('received message (on {})'.format(msg.topic))
 
     if msg.topic == MQTTTopicExecuteTask:
-        __execute_task(client, msg.payload)
+        task_request = json.loads(msg.payload)
+        log.i('task request: {}'.format(task_request))
+        # Look at the executor ID to see if the task is really for this instance.
+        our_id = data['executor_id']
+        if task_request['executer_id'] != data['executor_id']:
+            log.d('task not for this instance (ours: {} != requested: {})'.format(
+                our_id, task_request['executer_id']))
+        else:
+            __execute_task(client, msg.payload)
 
-def __execute_task(client, task_request_json):
+def __execute_task(client, task_request):
     '''Begin executing a task in a new thread.
 
     Sends a response to the controller after completion.
     '''
 
-    task_request = json.loads(task_request_json)
-    threading.Thread(target=__executor_task_entry,
+    thread = threading.Thread(target=__executor_task_entry,
                      # Hm. Is the MQTT client thread-safe?
                      args=(client, task_request))
+    thread.start()
+
+    return thread
 
 def __executor_task_entry(mqtt_client, task_request):
     '''Task execution thread entry.
@@ -89,9 +102,9 @@ def __executor_task_entry(mqtt_client, task_request):
     # Collect current state and send it, along with the result.
     current_state = stats.fetch()
     response = {
-        'executor_id': task_request.executer_id,
-        'task_id': task_request.task_id,
-        'offload_id': task_request.offload_id,
+        'executor_id': task_request['executer_id'],
+        'task_id': task_request['task_id'],
+        'offload_id': task_request['offload_id'],
         'result': '', # TODO: pull this from the computed result.
         'state': current_state,
     }
