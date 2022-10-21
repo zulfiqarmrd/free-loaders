4de87a3cc5bc508faa41eaba357ff5c017588acc
Rabbi <f.rabbimasum@gmail.com>
2022-10-21 15:37:30 -0400

    scheduler WIP

--- controller/rl_scheduler.py
+++ controller/rl_scheduler.py
@@ -8,54 +8,13 @@ import copy
 import chainer
 import chainer.functions as F
 import chainer.links as L
-from plotly import tools
-from plotly.graph_objs import *
-from plotly.offline import init_notebook_mode, iplot, iplot_mpl
 import numpy as np
 
 
 class RLScheduler:
+
     def __init__(self, executers):
         self.executers = executers
-
-    def get_state(self, state, task):
-
-        #merge state and task into correct format
-        # To Do
-        updated_state = state+ task
-
-        return updated_state
-
-    def generate_new_state(before_task, new_state, exec_id):
-        # calculate new state
-
-        return 0
-
-
-    def get_before_state(self, offload_id):
-        # return state from the table
-        return 0
-
-    def generate_reward(self, deadline, exec_time):
-
-        if exec_time < deadline:
-            # TO_DO
-            reward = 1
-
-        else:
-            reward = -1
-
-        return reward
-
-
-    def done_with_learning(self, reward):
-
-        # To Do
-        #if reached plateau return 1, else return 0
-
-        return 0
-
-    def schedule(self, before_state, task):
 
         class Q_Network(chainer.Chain):
 
@@ -75,112 +34,154 @@ class RLScheduler:
             def reset(self):
                 self.zerograds()
 
-        # To_Do
-        Q = Q_Network(input_size=12, hidden_size=100, output_size=2)  # 4 features, 2 actions
-        Q_ast = copy.deepcopy(Q)
-        optimizer = chainer.optimizers.Adam()
-        optimizer.setup(Q)
+        self.total_executor = 10
 
-        epoch_num = 20
-        memory_size = 800
-        batch_size = 20
-        epsilon = 1.0
-        epsilon_decrease = 1e-3
-        epsilon_min = 0.1
-        start_reduce_epsilon = 200
-        train_freq = 10
-        update_q_freq = 20
-        gamma = 0.97
-        show_log_freq = 5
+        #ToDo, update input size
+        self.Q = Q_Network(input_size=12, hidden_size=100, output_size=self.total_executor)
 
-        memory = []
-        total_step = 0
-        total_rewards = []
-        total_losses = []
+        self.Q_ast = copy.deepcopy(self.Q)
+        self.optimizer = chainer.optimizers.Adam()
+        self.optimizer.setup(self.Q)
 
-        start = time.time()
-        #for epoch in range(epoch_num):
+        self.epoch_num = 20
+        self.memory_size = 800
+        self.batch_size = 20
+        self.epsilon = 1.0
+        self.epsilon_decrease = 1e-3
+        self.epsilon_min = 0.1
+        self.start_reduce_epsilon = 200
+        self.train_freq = 10
+        self.update_q_freq = 20
+        self.gamma = 0.97
+        self.show_log_freq = 5
 
-        pobs = self.get_state(before_state, task)
-        done = False
-        total_reward = 0
-        total_loss = 0
+        self.memory = []
+        self.total_step = 0
+        self.total_rewards = []
+        self.total_losses = []
 
-        #    while not done: #update
+        self.state_table = {}
+
+    def save_state(self, task, state, act):
+
+        self.state_table[task.offload_id] = [state, act, task.deadline]
+
+    def get_saved_state(self, offload_id):
+
+        state, act, deadline = self.state_table[offload_id]
+        del self.state_table[offload_id]
+
+        return state, act, deadline
+
+    # TODO: Process state and task into correct format
+    def process_state(self, state, task):
+
+        updated_state = state + task
+
+        return updated_state
+
+    # TODO: Get latest state from old state and exec_id
+    def generate_new_state(self, before_state, new_state, exec_id):
+        # calculate new state
+
+        return 0
+
+    # TODO: Update the reward function
+    def generate_reward(self, deadline, exec_time):
+
+        if exec_time < deadline:
+            reward = 1
+
+        else:
+            reward = -1
+
+        return reward
+
+    # TODO: When to stop? Optional for now
+    def done_with_learning(self, reward):
+
+        #if reached plateau return 1, else return 0
+
+        return 0
+
+    def schedule(self, before_state, task):
+
+        pobs = self.process_state(before_state, task)
 
         # select act
-        pact = np.random.randint(2) # update with executers id
-        if np.random.rand() > epsilon:
-            pact = Q(np.array(pobs, dtype=np.float32).reshape(1, -1))
+        pact = np.random.randint(self.total_executor)
+        if np.random.rand() > self.epsilon:
+            pact = self.Q(np.array(pobs, dtype=np.float32).reshape(1, -1))
             pact = np.argmax(pact.data)
+
+        self.save_state(task, before_state, pact)
 
         return pact
 
 
-    def task_finished(self, task, exec_time, new_state, exec_id):
+    def task_finished(self, offload_id, exec_time, new_state, exec_id):
 
-        before_state = self.get_before_state(task.offload_id)
+        pobs, pact, deadline = self.get_saved_state(offload_id)
 
-        obs = self.generate_new_state(before_state, new_state, exec_id)
-        reward = self.generate_reward(task.deadline, exec_time)
+        obs = self.generate_new_state(pobs, new_state, exec_id)
+        reward = self.generate_reward(deadline, exec_time)
         done = self.done_with_learning(reward)
 
+        # add memory
+        self.memory.append((pobs, pact, reward, obs, done))
+        if len(self.memory) > self.memory_size:
+            self.memory.pop(0)
 
-                # add memory
-                memory.append((pobs, pact, reward, obs, done))
-                if len(memory) > memory_size:
-                    memory.pop(0)
+        # train or update q
+        if len(self.memory) == self.memory_size:
 
-                # train or update q
-                if len(memory) == memory_size:
-                    if total_step % train_freq == 0:
-                        shuffled_memory = np.random.permutation(memory)
-                        memory_idx = range(len(shuffled_memory))
-                        for i in memory_idx[::batch_size]:
-                            batch = np.array(shuffled_memory[i:i + batch_size])
+            for epoch in range(self.epoch_num):
 
-                            b_pobs = np.array(batch[:, 0].tolist(), dtype=np.float32).reshape(batch_size, -1)
-                            b_pact = np.array(batch[:, 1].tolist(), dtype=np.int32)
+                total_reward = 0
+                total_loss = 0
 
-                            b_reward = np.array(batch[:, 2].tolist(), dtype=np.int32)
-                            b_obs = np.array(batch[:, 3].tolist(), dtype=np.float32).reshape(batch_size, -1)
-                            b_done = np.array(batch[:, 4].tolist(), dtype=np.bool)
-                            q = Q(b_pobs)
-                            maxq = np.max(Q_ast(b_obs).data, axis=1)
-                            target = copy.deepcopy(q.data)
-                            for j in range(batch_size):
-                                target[j, b_pact[j]] = b_reward[j] + gamma * maxq[j] * (not b_done[j])
-                            Q.reset()
-                            loss = F.mean_squared_error(q, target)
-                            total_loss += loss.data
-                            loss.backward()
-                            optimizer.update()
+                if self.total_step % self.train_freq == 0:
+                    shuffled_memory = np.random.permutation(self.memory)
+                    memory_idx = range(len(shuffled_memory))
+                    for i in memory_idx[::self.batch_size]:
+                        batch = np.array(shuffled_memory[i:i + self.batch_size])
 
-                    if total_step % update_q_freq == 0:
-                        Q_ast = copy.deepcopy(Q)
+                        b_pobs = np.array(batch[:, 0].tolist(), dtype=np.float32).reshape(self.batch_size, -1)
+                        b_pact = np.array(batch[:, 1].tolist(), dtype=np.int32)
+
+                        b_reward = np.array(batch[:, 2].tolist(), dtype=np.int32)
+                        b_obs = np.array(batch[:, 3].tolist(), dtype=np.float32).reshape(self.batch_size, -1)
+                        b_done = np.array(batch[:, 4].tolist(), dtype=np.bool)
+                        q = self.Q(b_pobs)
+                        maxq = np.max(Q_ast(b_obs).data, axis=1)
+                        target = copy.deepcopy(q.data)
+                        for j in range(self.batch_size):
+                            target[j, b_pact[j]] = b_reward[j] + self.gamma * maxq[j] * (not b_done[j])
+                        self.Q.reset()
+                        loss = F.mean_squared_error(q, target)
+                        total_loss += loss.data
+                        loss.backward()
+                        self.optimizer.update()
+
+                if self.total_step % self.update_q_freq == 0:
+                    Q_ast = copy.deepcopy(self.Q)
 
                 # epsilon
-                if epsilon > epsilon_min and total_step > start_reduce_epsilon:
-                    epsilon -= epsilon_decrease
+                if self.epsilon > self.epsilon_min and self.total_step > self.start_reduce_epsilon:
+                    self.epsilon -= self.epsilon_decrease
 
                 # next step
                 total_reward += reward
-                pobs = obs
-                total_step += 1
+                self.total_step += 1
 
-            total_rewards.append(total_reward)
-            total_losses.append(total_loss)
+            self.total_rewards.append(total_reward)
+            self.total_losses.append(total_loss)
 
-        #     if (epoch + 1) % show_log_freq == 0:
-        #         log_reward = sum(total_rewards[((epoch + 1) - show_log_freq):]) / show_log_freq
-        #         log_loss = sum(total_losses[((epoch + 1) - show_log_freq):]) / show_log_freq
-        #         elapsed_time = time.time() - start
-        #         print('\t'.join(map(str, [epoch + 1, epsilon, total_step, log_reward, log_loss, elapsed_time])))
-        #         start = time.time()
-        #
-        # return Q, total_losses, total_rewards
+            if (epoch + 1) % self.show_log_freq == 0:
+                log_reward = sum(self.total_rewards[((epoch + 1) - self.show_log_freq):]) / self.show_log_freq
+                log_loss = sum(self.total_losses[((epoch + 1) - self.show_log_freq):]) / self.show_log_freq
+                print('\t'.join(map(str, [epoch + 1, self.epsilon, self.total_step, log_reward, log_loss])))
 
-
-        return 0
+        # save Q, total_losses, total_rewards
 
 
