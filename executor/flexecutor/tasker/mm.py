76841cc36f9184498d2e17fa82e44493854ea53a
Nabeel Nasir <nabeeln7@gmail.com>
2022-10-28 15:09:07 -0400

    update mm task

--- executor/flexecutor/tasker/mm.py
+++ executor/flexecutor/tasker/mm.py
@@ -1,6 +1,5 @@
-import sys
-import numpy as np
 import json
+import numpy as np
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -13,16 +12,12 @@ class NumpyEncoder(json.JSONEncoder):
 def run_mm_task(input_data):
     a = np.array(input_data["a"])
     b = np.array(input_data["b"])
-    res = np.matmul(a, b)
+    res = np.matmul(a,b)
     json_dump = json.dumps(res, cls=NumpyEncoder)
     return json_dump
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("needs input_data as arg")
-        sys.exit(1)
+    a = np.random.rand(1000,1000)
+    b = np.random.rand(1000,1000)
     # code to generate matrix input in json format:
-    # a = np.random.rand(1000,1000)
-    # b = np.random.rand(1000,1000)
-    # print(json.dumps({"a": a, "b": b}, cls=NumpyEncoder))
-    run_mm_task(sys.argv[1])
+    print(run_mm_task({"a": a, "b": b}))
